4764d074973cb2518b50005c8b0ef7df2b938e9f
Guruprakash.G <guruprakash.g@hotmail.com>
2024-03-29 13:09:52 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product List</main>
+      <footer>MaanavaN Learn Code</footer>
+    </div>
+  );
+}
+
+export default App;
